ad7f4238fb69ecf9918e7747503e22e390774fdd
Midas Maas <I361913>
2022-07-08 15:24:07 +0200

    Banners en IMGs ingevoegd bij de over-pagina.

--- portfolio-m-maas-s6/src/views/AboutView.vue
+++ portfolio-m-maas-s6/src/views/AboutView.vue
@@ -39,10 +39,90 @@
 
       </div>
       <!--banner EINDE-->
+
+      <!--Avatar BEGIN-->
       <div class="avatar_container">
         <img class="avatar_IMG" src="../assets/avatar.png">
       </div>
-      <div class="avatar"></div>
+      <!--Avatar EINDE-->
+
+      <!--Journalistiek IMG BEGIN-->
+      <div class="journalistiek_IMG_container">
+        <img src="../assets/journalistiekObject.png">
+      </div>
+      <!--Journalistiek IMG EINDE-->
+
+      <!--Journalistiek BANNER BEGIN-->
+      <div class="journalistiek_banner_container">
+        <div class="headline_container_background_shadow"></div>
+
+        <div class="headline_container_background_outline">
+          <div class="headline_container_background_bar">
+            <div class="headline_container_background_bar_dot __right"></div>
+            <div class="headline_container_background_bar_dot"></div>
+            <div class="headline_container_background_bar_dot"></div>
+          </div>
+          <div class="headline__text">
+            <h2>
+              Voor ik mij bezighield met het web, had ik de ambitie om journalist te worden. Een diploma heb ik daar niet aan overgehouden, maar wel <span>gouden</span> ervaringen in de muziekjournalistiek, bij media als The Daily Indie en Aardschok. Hier leerde ik mijn ijver en brede interesse te smeden. Ik nam <span>schrijfvaardigheid</span> en <span>communicatieve vaardigheden</span> mee.
+            </h2>
+          </div>
+        </div>
+      </div>
+      <!--Journalistiek BANNER EINDE-->
+
+      <!--ICT BANNER BEGIN-->
+      <div class="ICT_banner_container">
+        <div class="headline_container_background_shadow"></div>
+
+        <div class="headline_container_background_outline">
+          <div class="headline_container_background_bar">
+            <div class="headline_container_background_bar_dot __right"></div>
+            <div class="headline_container_background_bar_dot"></div>
+            <div class="headline_container_background_bar_dot"></div>
+          </div>
+          <div class="headline__text">
+            <h2>
+              Het <span>goud</span> blonk echter niet zoals ik had gehoopt. Om ervoor te zorgen dat ik ooit een beetje als een vorst kan leven, ging ik andere paden verkennen. Dit verleide mij om ICT & Media Design te gaan studeren aan de Fontys in Tilburg, vanwege de interessante combinatie tussen techniek en creativiteit. Design tot leven brengen met code, is waar het bij mij van gaat kriebelen. 
+            </h2>
+          </div>
+        </div>
+      </div>
+
+      <!--ICT BANNER EINDE-->
+      
+      <!--ICT IMG BEGIN-->
+      <div class="ICT_IMG_container">
+        <img src="../assets/ICTIMG.png">
+      </div>
+      <!--ICT IMG EINDE-->
+
+      <!--Stage IMG BEGIN-->
+      <div class="Stage_IMG_container">
+        <img src="../assets/win98.png">
+      </div>
+      <!--Stage IMG EINDE-->
+
+      <!--Stage BANNER BEGIN-->
+      <div class="Stage_banner_container">
+        <div class="headline_container_background_shadow"></div>
+
+        <div class="headline_container_background_outline">
+          <div class="headline_container_background_bar">
+            <div class="headline_container_background_bar_dot __right"></div>
+            <div class="headline_container_background_bar_dot"></div>
+            <div class="headline_container_background_bar_dot"></div>
+          </div>
+          <div class="headline__text">
+            <h2>
+              Waar ik dan stiekem wel met tevredenheid op terugkijk, is mijn tijd als stagiair bij Partout Digital Agency. Hier had ik mijn bureau tussen de design- en developmentafdelingen in, waar ik mijn eigen projecten had, waarvan ik het meest trots ben op <span>deze</span> onboardingwebsite, die ik zelf ontwierp en vervolgens heb gerealiseerd in Vue.js. 
+            </h2>
+          </div>
+        </div>
+      </div>
+
+      <!--Stage BANNER EINDE-->
+
     </div>
   </div>
 </template>
