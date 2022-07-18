f50ee9693c559023833b7b87b843fdde6ed60acf
Midas Maas <I361913>
2022-07-18 12:11:38 +0200

    Begin gemaakt met SCSS van productpagina.

--- portfolio-m-maas-s6/src/views/PortfolioProductView.vue
+++ portfolio-m-maas-s6/src/views/PortfolioProductView.vue
@@ -1,17 +1,41 @@
 <template>
- <div class="Banner_container">
-        <div class="Banner_container_background_shadow"></div>
+<div class="container">
+    <div class="container_grid">
+<div class="CVInfo_container CVInfo_banner">
+        <div class="CVInfo_container_background_shadow"></div>
 
-        <div class="Banner_container_background_outline">
-          <div class="Banner_container_background_bar">
-            <div class="Banner_container_background_bar_dot __right"></div>
-            <div class="Banner_container_background_bar_dot"></div>
-            <div class="Banner_container_background_bar_dot"></div>
+        <div class="CVInfo_container_background_outline">
+          <div class="Banner__text">
+            <h2>Omroep Flevoland</h2>
+            <div class="_TagLine">
+              <p class="_TagLine_Text"><span>Nieuwe redactieportaal</span></p>
+              <span class="bar"></span>
+            </div>
+            <p></p>
           </div>
-          <div v-html="textForBanner" class="Banner__text">
+          
+        </div>
+        <div class="CVInfo_bottomBar_container">
+          <div class="CVInfo_bottomBar _BasisInfo">
+            <div class="CV_Info_bottomBar_alignDiv">
+              <p><span>Client</span></p>
+            <p>Omroep Flevoland</p>
+            </div>
+          </div>
+          <div class="CVInfo_bottomBar _DuurInfo">
+            <div class="CV_Info_bottomBar_alignDiv">
+            <p><span>Skills</span></p>
+            <p>UI/UX, research</p>
+            </div>
           </div>
         </div>
+
+        <p class="CVInfo_banner_Year">
+          2022
+        </p>
       </div>
+    </div>
+</div>
 </template>
 
 <script>
