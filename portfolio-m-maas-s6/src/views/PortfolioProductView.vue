77df4879740ad0388bc8d9fb0ddc62bf2d2fc36e
Midas Maas <I361913>
2022-07-25 21:25:38 +0200

    Productpagina SCSS afgemaakt.

--- portfolio-m-maas-s6/src/views/PortfolioProductView.vue
+++ portfolio-m-maas-s6/src/views/PortfolioProductView.vue
@@ -114,6 +114,127 @@
       <h1 class="ProductView_number">
         02.
       </h1>
+
+      <!--Het Proces banner-->
+
+      <div class="Banner_container ProcesBannerContainer">
+        <div class="Banner_container_background_shadow"></div>
+
+        <div class="Banner_container_background_outline">
+          <div class="Banner_container_background_bar">
+            <div class="Banner_container_background_bar_dot __right"></div>
+            <div class="Banner_container_background_bar_dot"></div>
+            <div class="Banner_container_background_bar_dot"></div>
+          </div>
+          <div class="DoelstellingContext_text">
+            <h1>Het</h1>
+            <h1>Proces</h1>
+
+            <div class="Doelstelling_bar"></div>
+
+            <p>Wat ik zoal gedaan heb voor het eindresultaat</p>
+          </div>
+        </div>
+      </div>
+
+      <!--Het Proces visual-->
+      <div class="ProcesVisual_container">
+        <div class="ProcesVisual_line">
+            <div class="ProcesVisual_square ProcesVisual_square_left">
+              <p>Expertinterviews</p>
+            </div>
+            <div class="ProcesVisual_square ProcesVisual_square_right">
+              <p>Userinterviews</p>
+            </div>
+            <div class="ProcesVisual_square ProcesVisual_square_left">
+              <p>Klikbaar design</p>
+            </div>
+            <div class="ProcesVisual_square ProcesVisual_square_right">
+              <p>Technische POC's</p>
+            </div>
+        </div>
+      </div>
+
+      <!-- Nummer -->
+      <h1 class="ProductView_number">
+        03.
+      </h1>
+      <!-- Conclusie banner -->
+      <div class="DoelstellingContext_container">
+        <div class="DoelstellingContext_container_background_shadow"></div>
+
+        <div class="DoelstellingContext_container_background_outline">
+          <div class="DoelstellingContext_container_background_bar">
+            <div
+              class="DoelstellingContext_container_background_bar_dot __right"
+            ></div>
+            <div class="DoelstellingContext_container_background_bar_dot"></div>
+            <div class="DoelstellingContext_container_background_bar_dot"></div>
+          </div>
+
+          <div class="DoelstellingContext_text_container">
+            <div class="Conclusie_div">
+              <div class="DoelstellingContext_text">
+                <h1>De</h1>
+                <h1>Conclusie</h1>
+
+                <div class="Conclusie_bar"></div>
+
+                <p>Uit als het onderzoek, trokken wij de volgende conclusie:</p>
+              </div>
+
+              <div class="DeDoelStelling_list">
+                <p>
+                  "Story based werken zorgt voor dat journalisten minder handelingen hoeven verrichten. Echter mist het hierdoor flexibiliteit die de journalist wenst. Als er meerdere ingangen zijn binnen een workflow, ontstaat er een vlottere, gebruiksvriendelijkere flow."
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Nummer -->
+      <h1 class="ProductView_number">
+        04.
+      </h1>
+
+      <!-- Resultaat banner -->
+      <div class="DoelstellingContext_container">
+        <div class="DoelstellingContext_container_background_shadow"></div>
+
+        <div class="DoelstellingContext_container_background_outline">
+          <div class="DoelstellingContext_container_background_bar">
+            <div
+              class="DoelstellingContext_container_background_bar_dot __right"
+            ></div>
+            <div class="DoelstellingContext_container_background_bar_dot"></div>
+            <div class="DoelstellingContext_container_background_bar_dot"></div>
+          </div>
+
+          <div class="DoelstellingContext_text_container">
+            <div class="Conclusie_div">
+              <div class="DoelstellingContext_text">
+                <h1>Het</h1>
+                <h1>Resultaat</h1>
+
+                <div class="Conclusie_bar"></div>
+
+                <p>Een nieuwe UI/UX die volledig past bij de wensen van de redacteurs van Omroep Flevoland: een die past bij de journalisten.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--CRT IMG-->
+      <div class="CVCrtIMG_container_fullWidth">
+        <img class="CVCrtIMG_banner" src="../assets/CRTOutline.png" />
+
+        <img
+          class="CVCrtIMG_menu_background"
+          src="../assets/ProductIMGOmroepFlevo.png"
+        />
+      </div>
     </div>
   </div>
 </template>
