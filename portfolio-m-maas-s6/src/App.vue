2f8b4cfcff8b140c7e8a7fb527b4196a2daead44
Midas Maas <I361913>
2022-09-21 16:47:29 +0200

    Websitenaam, favicon aangepast en product huisstijl toegevoegd.

--- portfolio-m-maas-s6/src/App.vue
+++ portfolio-m-maas-s6/src/App.vue
@@ -1,5 +1,10 @@
 <template>
 <div id="app">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Midas</title>
+</head>  
 <hamburger-menu></hamburger-menu>
     <router-view class="container_grid__nav_view"/>
   <footer-bottom></footer-bottom>
@@ -24,9 +29,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  
 }
 
 </style>
