9ed71a74510600714e1ee24e4de4c3ebfa4c7161
Midas Maas <I361913>
2022-09-26 21:51:54 +0200

    Bug fix: hamburger open wanneer pagina laat.

--- portfolio-m-maas-s6/src/components/HamburgerMenu.vue
+++ portfolio-m-maas-s6/src/components/HamburgerMenu.vue
@@ -9,7 +9,7 @@
           src="../assets/LogoHamburgerSVG.svg"
         />
         </router-link>
-        <a @click="hamburgerOpen = true" class="hamburger_button__parent">
+        <a @click="hamburgerAnim(true)" class="hamburger_button__parent">
           <div class="hamburger_button__parent_background"></div>
           <div class="hamburger_button__child">
             <div class="top"></div>
@@ -22,14 +22,14 @@
     <!--Hamburgermenu gesloten EINDE-->
 
     <!--Hamburgermenu open BEGIN-->
-    <div :class="hamburgerAnim" class="hamburger_container_OPEN">
-      <div :class="hamburgerAnimOpacity" class="hamburger_background_background"></div>
+    <div class="hamburger_container_OPEN close">
+      <div class="hamburger_background_background"></div>
       <div class="hamburger_background_shadow"></div>
       <div class="hamburger_background_container">
         <div class="hamburger_background_outline">
           <div class="hamburger_background_bar">
             <div
-              @click="hamburgerOpen = false"
+              @click="hamburgerAnim(false)"
               class="hamburger_background_bar_dot __right"
             >
               <div class="before"></div>
@@ -42,7 +42,7 @@
       </div>
       <nav class="hamburger_container_grid">
         <div class="hamburger_nav_container">
-          <div :class="hamburgerStagger" class="hamburger_nav_container__items">
+          <div class="hamburger_nav_container__items">
             <router-link class="dropdown_item-1" to="/">OVER</router-link>
             <router-link class="dropdown_item-2" :class="routePortfolioActive" to="/portfolio">PORTFOLIO</router-link>
             <router-link class="dropdown_item-3" :class="routeCVActive" to="/cv/0">CV</router-link>
@@ -58,17 +58,33 @@
 <script>
 export default {
   name: "HamburgerMenu",
-  data() {
-    return {
-      hamburgerOpen: false,
-    };
-  },
   watch: {
     $route(to, from) {
       if (to !== from) {
-        this.hamburgerOpen = false;
+        this.hamburgerAnim(false)
       }
     },
+  },
+  mounted(){
+  },
+  methods: {
+    hamburgerAnim(open){
+      let openDiv = document.getElementsByClassName('hamburger_container_OPEN')[0]
+      let bgDiv = document.getElementsByClassName('hamburger_background_background')[0]
+      if(open){
+        openDiv.classList.add('hamburgerDown', 'hamburger_stagger')
+        bgDiv.classList.add('hamburgerDownOpacity')
+        openDiv.classList.remove('hamburgerUp')
+        openDiv.classList.remove('close')
+      } else {
+        openDiv.classList.add('hamburgerUp')
+        openDiv.classList.remove('hamburgerDown', 'hamburger_stagger')
+        bgDiv.classList.remove('hamburgerDownOpacity')
+        setTimeout(() => {
+          openDiv.classList.add('close')
+        },300)
+      }
+    }
   },
   computed: {
     routeCVActive(){
@@ -80,21 +96,6 @@ export default {
     if (this.$route.params.productid >= 0){
       return 'router-link-exact-active'
     } return ''
-    },
-    hamburgerAnim(){
-      if(this.hamburgerOpen == true){
-        return 'hamburgerDown'
-      } else return 'hamburgerUp'
-    },
-    hamburgerStagger(){
-      if(this.hamburgerOpen == true){
-        return 'hamburger_stagger'
-      } else return ''
-    },
-    hamburgerAnimOpacity(){
-      if(this.hamburgerOpen == true){
-        return 'hamburgerDownOpacity'
-      } else return 'hamburgerUpOpacity'
     },
   },
 };
